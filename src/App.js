b95476a35300e4dd36866bc0a62b125bd7e786f0
969hadiahmed@gmail.com <969hadiahmed@gmail.com>
2023-07-12 11:31:51 +0530

    Logo Done, Nav Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import React from 'react';
 function App() {
   return (
     <div className="App">
       <Navigation/>
+      <Logo/>
       {
-      //<Logo/>
       //<ImageLinkForm/>
       //<FaceRecognition/> 
       }
