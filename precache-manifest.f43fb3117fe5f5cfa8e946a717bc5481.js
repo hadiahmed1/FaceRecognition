603ae78a6c7a79cb977571f54992c57fec5c140a
969hadiahmed@gmail.com <969hadiahmed@gmail.com>
2023-07-26 17:37:49 +0530

    Updates

---
+++ precache-manifest.f43fb3117fe5f5cfa8e946a717bc5481.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca892684b39b1873dc72",
+    "url": "/FaceRecognition/static/css/main.1fa446b6.chunk.css"
+  },
+  {
+    "revision": "ca892684b39b1873dc72",
+    "url": "/FaceRecognition/static/js/main.ca892684.chunk.js"
+  },
+  {
+    "revision": "3a79397ac1a1b3a66e79",
+    "url": "/FaceRecognition/static/js/1.3a79397a.chunk.js"
+  },
+  {
+    "revision": "293fab59f81b2123917f",
+    "url": "/FaceRecognition/static/css/2.000bfb2f.chunk.css"
+  },
+  {
+    "revision": "293fab59f81b2123917f",
+    "url": "/FaceRecognition/static/js/2.293fab59.chunk.js"
+  },
+  {
+    "revision": "1c086fb2b0e97522096b",
+    "url": "/FaceRecognition/static/js/runtime~main.1c086fb2.js"
+  },
+  {
+    "revision": "07564ab07a9cb91c6807f087748be9b7",
+    "url": "/FaceRecognition/index.html"
+  }
+];
